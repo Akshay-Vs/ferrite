96ccebc673de64987b40b10ba07005fb3e559cf5
Akshay-Vs <akvs.dev@gmail.com>
2026-03-03 02:08:11 +0530

    feat: configure cache, throttling, swagger & helmet

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -1,10 +1,47 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
+import { HttpExceptionFilter } from './common/filters/http-error-filter';
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		CacheModule.register({
+			ttl: 60000, // 60 sec
+			isGlobal: true,
+		}),
+
+		// Throttler
+		ThrottlerModule.forRoot({
+			throttlers: [
+				{
+					ttl: 60000, // 60 sec
+					limit: 10,
+				},
+			],
+		}),
+
+		// Config
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+	],
+	controllers: [],
+	providers: [
+		{
+			provide: APP_PIPE,
+			useClass: ZodValidationPipe,
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: ZodSerializerInterceptor,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: HttpExceptionFilter,
+		},
+	],
 })
 export class AppModule {}
--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -1,8 +1,53 @@
+import { Logger as NestLogger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import type { NextFunction } from 'express';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
+
+const PORT = process.env.PORT ?? 4000;
+const VERSION = process.env.VERSION ?? 'v1';
+const ORIGIN = process.env.ORIGIN_URL?.trim()
+	? process.env.ORIGIN_URL.trim().split(/\s+/)
+	: [];
+
+NestLogger.debug(
+	`Using env: ${JSON.stringify({ port: PORT, origin: ORIGIN }, null, 2)}`,
+	'GLOBAL'
+);
+
+const logger = new NestLogger('Bootstrap');
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+
+	app.use(helmet());
+	app.enableCors({
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		origin: ORIGIN,
+		credentials: true,
+	});
+
+	app.setGlobalPrefix(VERSION);
+
+	app.use((req: Request, _res: Response, next: NextFunction) => {
+		logger.debug(`Request: ${req.method} ${req.url} received`);
+		next();
+	});
+
+	setupSwagger(app);
+
+	await app.listen(PORT);
+	NestLogger.log(`Application Port: ${PORT}`, '');
 }
-bootstrap();
+
+void (async (): Promise<void> => {
+	try {
+		await bootstrap();
+		logger.log(
+			`Server Started in ${process.env.NODE_ENV ?? 'PRODUCTION'} mode`
+		);
+	} catch (error) {
+		logger.error(error);
+	}
+})();
