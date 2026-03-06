421e4e6418410c2a64aeb3401aec496a190a527c
Akshay-Vs <akvs.dev@gmail.com>
2026-03-06 23:23:23 +0530

    feat: implement VerifyTokenUsecase for token verification and error handling

--- apps/core/src/modules/auth/application/use-cases/verify-token.usecase.ts
+++ apps/core/src/modules/auth/application/use-cases/verify-token.usecase.ts
@@ -1,1 +1,25 @@
-// calls authProvider.verifyToken() & returns AuthUser
+import { AppLogger } from '@core/logger/logger.service';
+import {
+	AUTH_PROVIDER,
+	AuthUser,
+	type IAuthProvider,
+} from '@modules/auth/domain/ports/auth-provider.port';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class VerifyTokenUsecase {
+	constructor(
+		@Inject(AUTH_PROVIDER) private authProvider: IAuthProvider,
+		private logger: AppLogger
+	) {}
+
+	async execute(token: string): Promise<AuthUser> {
+		try {
+			const user = await this.authProvider.verifyToken(token);
+			return user;
+		} catch (e) {
+			this.logger.error(e);
+			throw new Error('Unable to verify token');
+		}
+	}
+}
