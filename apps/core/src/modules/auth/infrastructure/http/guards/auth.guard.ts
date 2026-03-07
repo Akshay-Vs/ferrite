03128fdf629eeaf0b808911dfd304e73fe5d7743
Akshay-Vs <akvs.dev@gmail.com>
2026-03-07 20:52:06 +0530

    feat: implement AuthGuard to manage route access and token verification

---
+++ apps/core/src/modules/auth/infrastructure/http/guards/auth.guard.ts
@@ -0,0 +1,66 @@
+import { Request } from '@common/types/request';
+import { AppLogger } from '@core/logger/logger.service';
+import { VerifyTokenUsecase } from '@modules/auth/application/use-cases/verify-token.usecase';
+import {
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_ROUTE } from '../decorators/public-route.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+	constructor(
+		private readonly logger: AppLogger,
+		private readonly reflector: Reflector,
+		private readonly verifyToken: VerifyTokenUsecase
+	) {
+		this.logger.setContext(AuthGuard.name);
+	}
+
+	private extractToken(request: Request): string | null {
+		const [type, token] = request.headers?.authorization?.split(' ') ?? [];
+
+		this.logger.debug(`Extracted token type: ${type}`);
+		return type === 'Bearer' ? token : null;
+	}
+
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		this.logger.debug('AuthGuard.canActivate');
+
+		const request: Request = context.switchToHttp().getRequest();
+
+		const isPublic = this.reflector.getAllAndOverride<boolean>(
+			IS_PUBLIC_ROUTE,
+			[context.getHandler(), context.getClass()]
+		);
+
+		if (isPublic) {
+			this.logger.debug('Public endpoint, bypassing auth');
+			return true;
+		}
+
+		const token = this.extractToken(request);
+
+		if (!token) {
+			this.logger.debug('No token provided, unauthorized');
+			throw new UnauthorizedException('No token provided');
+		}
+
+		const authUser = await this.verifyToken.execute(token);
+
+		if (authUser.isErr()) {
+			this.logger.error('Failed to verify token');
+			throw new UnauthorizedException(authUser.error.message);
+		}
+
+		this.logger.debug(
+			`Request ${request.path} authorized as ${authUser.value.email}`
+		);
+
+		request.authUser = authUser.value;
+		return true;
+	}
+}
