46b9fb37b81309e826e9760ebaf49ddd7eabac05
Akshay-Vs <akvs.dev@gmail.com>
2026-03-15 00:08:40 +0530

    feat: export tracer & queue module

--- apps/core/src/core/core.module.ts
+++ apps/core/src/core/core.module.ts
@@ -2,11 +2,27 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/db.module';
 import { LoggerModule } from './logger/logger.module';
+import { QueueModule } from './queue/module';
 import { RequestContextModule } from './request-context/request-context.module';
+import { TracerModule } from './tracer/tracer.module';
 
 @Global()
 @Module({
-	imports: [RequestContextModule, LoggerModule, ConfigModule, DatabaseModule],
-	exports: [RequestContextModule, LoggerModule, ConfigModule, DatabaseModule],
+	imports: [
+		RequestContextModule,
+		LoggerModule,
+		TracerModule,
+		ConfigModule,
+		DatabaseModule,
+		QueueModule,
+	],
+	exports: [
+		RequestContextModule,
+		LoggerModule,
+		TracerModule,
+		ConfigModule,
+		DatabaseModule,
+		QueueModule,
+	],
 })
 export class CoreModule {}
