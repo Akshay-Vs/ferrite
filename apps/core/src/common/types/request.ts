a3eb55b30a6d94836fdb3c3ec387f444075d1012
Akshay-Vs <akvs.dev@gmail.com>
2026-03-15 00:09:35 +0530

    feat: add webhook request type

--- apps/core/src/common/types/request.ts
+++ apps/core/src/common/types/request.ts
@@ -1,4 +1,4 @@
-import { AuthUser } from '@auth/domain/schemas';
+import { AuthUser, RawWebhookClaims } from '@auth/domain/schemas';
 import { Request as ExpressRequest } from 'express';
 
 export interface Request extends ExpressRequest {
@@ -7,4 +7,8 @@ export interface Request extends ExpressRequest {
 
 export interface AuthenticatedRequest extends Request {
 	authUser: AuthUser;
+}
+
+export interface WebhookRequest extends Request {
+	rawClaims?: RawWebhookClaims;
 }
