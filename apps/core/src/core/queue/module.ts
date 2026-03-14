2498e229aba1ea4447b553e023ed2a922cbf8cc3
Akshay-Vs <akvs.dev@gmail.com>
2026-03-15 00:05:57 +0530

    feat: implement base consumer and producer classes with queue module and default job options

---
+++ apps/core/src/core/queue/module.ts
@@ -0,0 +1,20 @@
+import { BullModule } from '@nestjs/bullmq';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+	imports: [
+		BullModule.forRootAsync({
+			useFactory: (config: ConfigService) => ({
+				connection: {
+					host: config.getOrThrow<string>('REDIS_HOST'),
+					port: config.getOrThrow<number>('REDIS_PORT'),
+					password: config.getOrThrow<string>('REDIS_PASSWORD'),
+				},
+			}),
+			inject: [ConfigService],
+		}),
+	],
+	exports: [BullModule],
+})
+export class QueueModule {}
