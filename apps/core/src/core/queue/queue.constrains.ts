bba3fde2949cf4805b10f3b83840d3ede1254f95
Akshay-Vs <akvs.dev@gmail.com>
2026-03-16 19:02:11 +0530

    feat: add failed job limit and ttl

--- apps/core/src/core/queue/queue.constrains.ts
+++ apps/core/src/core/queue/queue.constrains.ts
@@ -4,5 +4,5 @@ export const DEFAULT_JOB_OPTIONS: DefaultJobOptions = {
 	attempts: 3,
 	backoff: { type: 'exponential', delay: 1000 },
 	removeOnComplete: true,
-	removeOnFail: false,
+	removeOnFail: { count: 5000, age: 7 * 24 * 60 * 60 }, // keep 5k failed jobs (atmost) for 7 days
 };
