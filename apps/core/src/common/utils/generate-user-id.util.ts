3efa27a37064a894fa5d7b9b2e84be75846e0ca6
Akshay-Vs <akvs.dev@gmail.com>
2026-03-19 01:57:58 +0530

    feat: add uuid validation to generate user id util

--- apps/core/src/common/utils/generate-user-id.util.ts
+++ apps/core/src/common/utils/generate-user-id.util.ts
@@ -1,7 +1,11 @@
-import { v5 as uuidv5 } from 'uuid';
+import { validate as isUuid, v5 as uuidv5 } from 'uuid';
 
 export type GenerateUserId = (externalId: string) => string;
 
 export function createUserIdGenerator(privateKey: string): GenerateUserId {
+	if (!isUuid(privateKey)) {
+		throw new Error('Invalid private key');
+	}
+
 	return (externalId: string) => uuidv5(externalId, privateKey);
 }
