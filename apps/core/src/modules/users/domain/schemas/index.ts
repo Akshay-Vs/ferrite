e81adbc8ba72c5723e48042bcdd50c9a1e697f25
Akshay-Vs <akvs.dev@gmail.com>
2026-03-18 16:06:25 +0530

    refactor: streamlined exports

---
+++ apps/core/src/modules/users/domain/schemas/index.ts
@@ -0,0 +1,6 @@
+export * from './update-profile.zodschema';
+export * from './user-created.zodschema';
+export * from './user-deleted.zodschema';
+export * from './user-profile.zodschema';
+export * from './user-sync-event.zodschema';
+export * from './user-updated.zodschema';
