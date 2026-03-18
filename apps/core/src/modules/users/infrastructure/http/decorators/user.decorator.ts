87ca233769075064176d229016c3f8ef6a7ee2ef
Akshay-Vs <akvs.dev@gmail.com>
2026-03-18 16:05:59 +0530

    refactor: remove lagacy user object decorator

--- apps/core/src/modules/users/infrastructure/http/decorators/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { AuthUser } from '@auth/index';
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const User = createParamDecorator(
-	(_data: unknown, ctx: ExecutionContext) => {
-		const request = ctx.switchToHttp().getRequest();
-		return request.authUser as AuthUser;
-	}
-);
