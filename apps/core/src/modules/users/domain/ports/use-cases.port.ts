26b16529748944b18aadff1fff9c98e74d99a001
Akshay-Vs <akvs.dev@gmail.com>
2026-03-19 20:51:24 +0530

    feat: implement RouteUserEventsUsecase for handling user sync events

--- apps/core/src/modules/users/domain/ports/use-cases.port.ts
+++ apps/core/src/modules/users/domain/ports/use-cases.port.ts
@@ -3,6 +3,7 @@ import { IUseCase } from '@common/interfaces/use-case.interface';
 import { UserConflictError } from '../errors/user-conflict.error';
 import { UserExistsError } from '../errors/user-exists.error';
 import { UserNotFoundError } from '../errors/user-not-found.error';
+import { UserSyncEvent } from '../schemas';
 import type { UpdateProfileInput } from '../schemas/update-profile.zodschema';
 import { UserCreatedEvent } from '../schemas/user-created.zodschema';
 import type {
@@ -14,7 +15,7 @@ import { UserUpdatedEvent } from '../schemas/user-updated.zodschema';
 export const CREATE_USER_UC = Symbol('CREATE_USER_UC');
 export const UPDATE_USER_UC = Symbol('UPDATE_USER_UC');
 export const DELETE_USER_UC = Symbol('DELETE_USER_UC');
-
+export const ROUTE_USER_EVENTS_UC = Symbol('ROUTE_USER_EVENTS_UC');
 export const GET_OWN_PROFILE_UC = Symbol('GET_OWN_PROFILE_UC');
 export const UPDATE_OWN_PROFILE_UC = Symbol('UPDATE_OWN_PROFILE_UC');
 
@@ -23,11 +24,13 @@ export type ICreateUserUseCase = IUseCase<
 	void,
 	UserExistsError | UserConflictError
 >;
+
 export type IUpdateUserUseCase = IUseCase<
 	UserUpdatedEvent,
 	void,
 	UserNotFoundError
 >;
+
 export type IDeleteUserUseCase = IUseCase<AuthUser, boolean, UserNotFoundError>;
 
 export type IGetUserProfileUseCase = IUseCase<
@@ -35,13 +38,17 @@ export type IGetUserProfileUseCase = IUseCase<
 	UserProfileBase,
 	UserNotFoundError
 >;
+
 export type IGetOwnProfileUseCase = IUseCase<
 	AuthUser,
 	UserProfileFull,
 	UserNotFoundError
 >;
+
 export type IUpdateOwnProfileUseCase = IUseCase<
 	{ authUser: AuthUser; data: UpdateProfileInput },
 	UserProfileFull,
 	UserNotFoundError
 >;
+
+export type IRouteUserEventsUseCase = IUseCase<UserSyncEvent, void, Error>;
