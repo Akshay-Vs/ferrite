18a805c12f30c75b6198cd4a4cd1db63d3b474d1
Akshay-Vs <akvs.dev@gmail.com>
2026-03-22 02:54:43 +0530

    feat(outbox): integrate OutboxProducer and update OutboxModule for event handling

--- apps/core/src/modules/outbox/index.ts
+++ apps/core/src/modules/outbox/index.ts
@@ -1,8 +1,11 @@
-export type { IOutboxDispatcher } from './domain/ports/outbox-dispatcher.port';
-export { OUTBOX_DISPATCHER } from './domain/ports/outbox-dispatcher.port';
+export type {
+	IInsertOutboxEvent,
+	OutboxInput,
+} from '@modules/outbox/domain/ports/outbox-usecases.port';
+export {
+	INSERT_OUTBOX_EVENT_UC,
+	InsertOutboxEventUseCase,
+} from './application/use-cases/insert-outbox-event.usecase';
 export type { IOutboxRepository } from './domain/ports/outbox-repository.port';
 export { OUTBOX_REPOSITORY } from './domain/ports/outbox-repository.port';
-export type { DomainEvent } from './domain/schemas/domain-event';
-export type { NewOutboxEventInput } from './domain/schemas/outbox-event.zodschema';
-export { newOutboxEventSchema } from './domain/schemas/outbox-event.zodschema';
 export { OutboxModule } from './outbox.module';
--- apps/core/src/modules/outbox/outbox.module.ts
+++ apps/core/src/modules/outbox/outbox.module.ts
@@ -1,22 +1,46 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
-import { OUTBOX_DISPATCHER } from './domain/ports/outbox-dispatcher.port';
+import {
+	INSERT_OUTBOX_EVENT_UC,
+	InsertOutboxEventUseCase,
+} from './application/use-cases/insert-outbox-event.usecase';
+import { OUTBOX_PRODUCER } from './domain/ports/outbox-producer.port';
 import { OUTBOX_REPOSITORY } from './domain/ports/outbox-repository.port';
 import { DrizzleOutboxRepository } from './infrastructure/persistance/drizzle-outbox.repository';
-import { OutboxDispatcherQueue } from './infrastructure/queue/outbox-dispatcher.queue';
-import { OutboxWorker } from './infrastructure/workers/outbox.worker';
+import { OutboxProducer } from './infrastructure/queue/outbox.producer';
+import { OutboxCDCWorker } from './infrastructure/replication/outbox-cdc.worker';
+import { OutboxReplicationSetupService } from './infrastructure/replication/outbox-replication-setup.service';
 
 @Module({
+	imports: [
+		BullModule.registerQueue({
+			name: 'default',
+		}),
+	],
 	providers: [
+		// use case
+		InsertOutboxEventUseCase,
+		// port bindings
 		{
 			provide: OUTBOX_REPOSITORY,
 			useClass: DrizzleOutboxRepository,
 		},
 		{
-			provide: OUTBOX_DISPATCHER,
-			useClass: OutboxDispatcherQueue,
+			provide: OUTBOX_PRODUCER,
+			useClass: OutboxProducer,
 		},
-		OutboxWorker,
+		{
+			provide: INSERT_OUTBOX_EVENT_UC,
+			useClass: InsertOutboxEventUseCase,
+		},
+		// infrastructure
+		OutboxReplicationSetupService,
+		OutboxCDCWorker,
 	],
-	exports: [OUTBOX_REPOSITORY],
+	exports: [
+		InsertOutboxEventUseCase,
+		OUTBOX_REPOSITORY,
+		INSERT_OUTBOX_EVENT_UC,
+	],
 })
 export class OutboxModule {}
