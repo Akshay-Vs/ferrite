eca66ce174a28d6bcdc4ade873356ddb87df4390
Akshay-Vs <akvs.dev@gmail.com>
2026-03-22 02:47:20 +0530

    refactor(database): streamline database module and provider, remove unused subscriber logic

--- apps/core/src/core/database/db.shutdown.ts
+++ apps/core/src/core/database/db.shutdown.ts
@@ -1,13 +1,12 @@
 import { AppLogger } from '@core/logger/logger.service';
 import { BeforeApplicationShutdown, Inject, Injectable } from '@nestjs/common';
 import type { Sql } from 'postgres';
-import { DB_CLIENT, SUBSCRIBER_CLIENT } from './db.provider';
+import { DB_CLIENT } from './db.provider';
 
 @Injectable()
 export class DatabaseShutdownService implements BeforeApplicationShutdown {
 	constructor(
 		@Inject(DB_CLIENT) private readonly dbClient: Sql,
-		@Inject(SUBSCRIBER_CLIENT) private readonly subscriberClient: Sql,
 		private readonly logger: AppLogger
 	) {
 		this.logger.setContext(DatabaseShutdownService.name);
@@ -15,7 +14,7 @@ export class DatabaseShutdownService implements BeforeApplicationShutdown {
 
 	async beforeApplicationShutdown() {
 		this.logger.log('Closing database connections…');
-		await Promise.all([this.dbClient.end(), this.subscriberClient.end()]);
+		await Promise.all([this.dbClient.end()]);
 		this.logger.log('Database connections closed.');
 	}
 }
