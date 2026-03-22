65fff4a1e34d7430d097cc24fb67fa481601d424
Akshay-Vs <akvs.dev@gmail.com>
2026-03-22 15:52:12 +0530

    refactor(route-user-events): enhance event handling by adding delete user use case

--- apps/core/src/modules/users/application/use-case/route-user-events.usercase.ts
+++ apps/core/src/modules/users/application/use-case/route-user-events.usercase.ts
@@ -1,30 +1,42 @@
-import { err, Result } from '@common/interfaces/result.interface';
+import { err, ok, Result } from '@common/interfaces/result.interface';
 import { AppLogger } from '@core/logger/logger.service';
 import { UnsupportedEventTypeError } from '@core/queue/queue.errors';
 import { Inject, Injectable } from '@nestjs/common';
 import {
 	CREATE_USER_UC,
+	DELETE_USER_UC,
 	type ICreateUserUseCase,
+	type IDeleteUserUseCase,
 	type IRouteUserEventsUseCase,
 } from '@users/domain/ports/use-cases.port';
-import { type UserSyncEvent } from '@users/domain/schemas';
 
 @Injectable()
 export class RouteUserEventsUsecase implements IRouteUserEventsUseCase {
 	constructor(
 		private readonly logger: AppLogger,
-		@Inject(CREATE_USER_UC) private readonly createUser: ICreateUserUseCase
+		@Inject(CREATE_USER_UC) private readonly createUser: ICreateUserUseCase,
+		@Inject(DELETE_USER_UC) private readonly deleteUser: IDeleteUserUseCase
 	) {
 		this.logger.setContext(this.constructor.name);
 	}
-	async execute(userSyncEvent: UserSyncEvent): Promise<Result<void, Error>> {
-		// Dispatch to use-case
-		const eventType = userSyncEvent.eventType;
+	async execute(rawEvent: any): Promise<Result<void, Error>> {
+		// Extract eventType
+		const eventType = rawEvent?.eventType ?? rawEvent?.type;
 
 		switch (eventType) {
 			case 'user.created':
 				this.logger.debug(`hit user.created event`);
-				return this.createUser.execute(userSyncEvent);
+				return this.createUser.execute(rawEvent);
+
+			case 'user.deleted': {
+				this.logger.debug(`hit user.deleted event`);
+				const result = await this.deleteUser.execute(rawEvent);
+				if (result.isErr()) {
+					return err(result.error);
+				}
+				return ok();
+			}
+
 			default: {
 				this.logger.warn(`Unsupported event type: ${eventType}`);
 				return err(new UnsupportedEventTypeError(eventType));
