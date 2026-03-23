b20fa0d7d5532ba023b9364a576c0db2380af223
Akshay-Vs <akvs.dev@gmail.com>
2026-03-23 15:21:18 +0530

    chore: format code

--- apps/core/src/modules/auth/infrastructure/adapters/providers/clerk.ts
+++ apps/core/src/modules/auth/infrastructure/adapters/providers/clerk.ts
@@ -201,7 +201,7 @@ export class ClerkAdapter implements ITokenAuth, IWebhookAuth, IDeleteUser {
 	}
 
 	async deleteUser(externalAuthId: string): Promise<void> {
-		return this.tracer.withSpan('adapters.clerk.deleteUser', async (span) => {
+		return this.tracer.withSpan('adapters.clerk.deleteUser', async (_span) => {
 			try {
 				await this.clerkClient.users.deleteUser(externalAuthId);
 			} catch (error) {
