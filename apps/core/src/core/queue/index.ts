21cb3aaf3dcd27fd99fe7deacef9e99564b5384e
Akshay-Vs <akvs.dev@gmail.com>
2026-04-03 11:06:34 +0530

    refactor(queue): remove base consumer, producer, module, constraints, and error classes

--- apps/core/src/core/queue/index.ts
+++
@@ -1,3 +0,0 @@
-export { BaseConsumer } from './base.consumer';
-export { BaseProducer } from './base.producer';
-export { DEFAULT_JOB_OPTIONS } from './queue.constraints';
--- apps/core/src/core/queue/queue.errors.ts
+++
@@ -1,6 +0,0 @@
-export class UnsupportedEventTypeError extends Error {
-	constructor(eventType: string) {
-		super(`Event type not supported: ${eventType}`);
-		this.name = 'UnsupportedEventTypeError';
-	}
-}
