f9b2c021c93a2855a0b1bbef7ca4c4ac3ecfd8d1
Akshay-Vs <akvs.dev@gmail.com>
2026-04-14 01:24:25 +0530

    fix: update RequirePermission decorator to use PermissionKey type

--- apps/core/src/common/decorators/require-permission.decorator.ts
+++ apps/core/src/common/decorators/require-permission.decorator.ts
@@ -1,6 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
+import type { PermissionKey } from '../schemas/permissions.zodschema';
 
 export const PERMISSIONS_KEY = Symbol('PERMISSIONS_KEY');
 
-export const RequirePermission = (...permissions: string[]) =>
+export const RequirePermission = (...permissions: PermissionKey[]) =>
 	SetMetadata(PERMISSIONS_KEY, permissions);
