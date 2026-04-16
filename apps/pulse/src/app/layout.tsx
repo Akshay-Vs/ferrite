9ff43f6bc0c5e179d94b7ccdb27e43d4247f3bf7
Akshay-Vs <akvs.dev@gmail.com>
2026-04-16 16:49:09 +0530

    feat: add initial layout, fonts, and metadata configuration

---
+++ apps/pulse/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import NextTopLoader from 'nextjs-toploader';
+import { inter } from '@/app/(config)/fonts';
+import { cn } from '@/core/utils/utils';
+import { appMetadata } from './(config)/metadata';
+
+import '@/presentation/styles/tailwind.css';
+import '@/presentation/styles/globals.scss';
+
+export const metadata: Metadata = appMetadata;
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<ClerkProvider>
+			<html lang="en" className={cn('font-sans', inter.variable)}>
+				<body className={`antialiased bg-bg`}>
+					<div className="min-h-dvh w-dvw p-6">
+						<NextTopLoader color="#A68BF8FA" height={2} showSpinner={false} />
+						{children}
+					</div>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
+}
---
+++ apps/pulse/src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { redirect } from 'next/navigation';
+
+const rootPage = () => {
+	return redirect('/overview');
+};
+
+export default rootPage;
