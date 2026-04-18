b16fd899460b1c47e1bc6b5ee76ad8cdb4cd32d9
Akshay-Vs <akvs.dev@gmail.com>
2026-04-18 19:03:43 +0530

    feat: add Facebook, Google, Microsoft, and Pulse icon components

---
+++ apps/pulse/src/presentation/shapes/microsoft-icon.tsx
@@ -0,0 +1,19 @@
+const MicrosoftIcon = () => {
+	return (
+		<svg
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			aria-label="Microsoft Logo"
+		>
+			<path d="M10.5 10.5H19.25V19.25H10.5V10.5Z" fill="white" />
+			<path d="M0 10.5H8.75V19.25H0V10.5Z" fill="white" />
+			<path d="M10.5 0H19.25V8.75H10.5V0Z" fill="white" />
+			<path d="M0 0H8.75V8.75H0V0Z" fill="white" />
+		</svg>
+	);
+};
+
+export default MicrosoftIcon;
