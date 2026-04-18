526f74608bbfbe28f9ed8ca9f4dc10832ee42731
Akshay-Vs <akvs.dev@gmail.com>
2026-04-18 19:11:05 +0530

    feat: restructure layout components and implement authentication middleware

--- apps/pulse/src/app/layout.tsx
+++ apps/pulse/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { appMetadata } from '@app/(config)/metadata';
 import { ClerkProvider } from '@clerk/nextjs';
 import { cn } from '@core/utils/utils';
 import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
 import NextTopLoader from 'nextjs-toploader';
 
 import '@presentation/styles/tailwind.css';
@@ -16,15 +17,23 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider>
-			<html lang="en" className={cn('font-sans', inter.variable)}>
-				<body className={`antialiased bg-bg`}>
-					<div className="min-h-dvh w-dvw p-6">
-						<NextTopLoader color="#A68BF8FA" height={3} showSpinner={false} />
-						{children}
+		<html lang="en" className={cn('font-sans', inter.variable)}>
+			<body className="antialiased bg-background text-content">
+				<ClerkProvider>
+					<div className="min-h-dvh w-dvw flex flex-col">
+						<ThemeProvider
+							attribute="class"
+							defaultTheme="dark"
+							enableSystem={false}
+							disableTransitionOnChange
+						>
+							<NextTopLoader color="#A68BF8FA" height={3} showSpinner={false} />
+
+							{children}
+						</ThemeProvider>
 					</div>
-				</body>
-			</html>
-		</ClerkProvider>
+				</ClerkProvider>
+			</body>
+		</html>
 	);
 }
