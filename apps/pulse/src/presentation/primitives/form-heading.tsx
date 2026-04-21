dabeabd65db4841f05e14d42d99beda2cef470c2
Akshay Vs <akvs.dev@gmail.com>
2026-04-21 23:09:08 +0530

    Update apps/pulse/src/presentation/primitives/form-heading.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/pulse/src/presentation/primitives/form-heading.tsx
+++ apps/pulse/src/presentation/primitives/form-heading.tsx
@@ -16,7 +16,7 @@ const FormHeading = ({
 			<h1 className="text-4xl font-extralight tracking-[0.012rem]">
 				{title} <GradientText text={highlightedText} className="font-light" />
 			</h1>
-			<p className="text- text-lg font-light">{description}</p>
+			<p className="text-lg font-light">{description}</p>
 		</div>
 	);
 };
