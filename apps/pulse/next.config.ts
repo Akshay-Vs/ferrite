97e17a2de5fee8f4df0e24f4d1252db5af5c96dc
Akshay-Vs <akvs.dev@gmail.com>
2026-04-23 20:09:51 +0530

    feat: update next.config to include remote image patterns & add new radius variable in tailwind.css

--- apps/pulse/next.config.ts
+++ apps/pulse/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'img.clerk.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.pexels.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'ik.imagekit.io',
+				port: '',
+				pathname: '/0ujcywips/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
