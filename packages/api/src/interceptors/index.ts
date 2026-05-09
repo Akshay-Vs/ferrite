1a431a08f7509b96bfdd7eb55f85da8955586deb
Akshay-Vs <akvs.dev@gmail.com>
2026-05-09 20:36:22 +0530

    feat: add index files for client, interceptors, interfaces, and services

---
+++ packages/api/src/interceptors/index.ts
@@ -0,0 +1,2 @@
+export * from './handle-http-error.interceptor';
+export * from './inject-auth-header.interceptor';
---
+++ packages/api/src/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './ferrite-client.port';
+export * from './ferrite-config.interface';
+export * from './request-config.interface';
