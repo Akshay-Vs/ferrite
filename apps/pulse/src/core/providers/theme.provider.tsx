319835043870db50d722c15f06877c392545f2a2
Akshay-Vs <akvs.dev@gmail.com>
2026-05-11 12:07:23 +0530

    feat: refactor ThemeProvider to use PropsWithChildren and simplify props handling

--- apps/pulse/src/core/providers/theme.provider.tsx
+++ apps/pulse/src/core/providers/theme.provider.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type * as React from 'react';
+import type { PropsWithChildren } from 'react';
 
-export function ThemeProvider({
-	children,
-	...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
+const ThemeProvider = ({ children }: PropsWithChildren) => {
+	return (
+		<NextThemesProvider
+			attribute="class"
+			defaultTheme="dark"
+			enableSystem={false}
+			disableTransitionOnChange
+		>
+			{children}
+		</NextThemesProvider>
+	);
+};
+
+export default ThemeProvider;
