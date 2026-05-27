1438121433beaf5f0aa82dc1e5b96a1d1c39bf9c
Akshay-Vs <akvs.dev@gmail.com>
2026-05-27 09:56:29 +0530

    refactor(queue): replace DrizzleTransaction with ITransactionContext accross queue module

--- apps/core/src/modules/queue/domain/ports/queue.reposotory.port.ts
+++ apps/core/src/modules/queue/domain/ports/queue.reposotory.port.ts
@@ -1,8 +1,8 @@
-import { DrizzleTransaction } from '@core/database/db.type';
+import { ITransactionContext } from '@common/interfaces/unit-of-work.interface';
 import { QueueParams } from '../schemas/queue-event.zodschema';
 
 export const QUEUE_REPOSITORY = Symbol('QUEUE_REPOSITORY');
 
 export interface IQueueRepository {
-	enqueue(tx: DrizzleTransaction, queueParams: QueueParams): Promise<void>;
+	enqueue(tx: ITransactionContext, queueParams: QueueParams): Promise<void>;
 }
--- apps/core/src/modules/queue/domain/ports/use-cases.port.ts
+++ apps/core/src/modules/queue/domain/ports/use-cases.port.ts
@@ -1,12 +1,12 @@
 import type { Result } from '@common/interfaces/result.interface';
-import type { DrizzleTransaction } from '@core/database/db.type';
+import type { ITransactionContext } from '@common/interfaces/unit-of-work.interface';
 import type { QueueParams } from '@modules/queue/domain/schemas/queue-event.zodschema';
 
 export const ENQUEUE_GRAPHILE_EVENT_UC = Symbol('ENQUEUE_GRAPHILE_EVENT');
 
 export interface IEnqueue {
 	execute(
-		tx: DrizzleTransaction,
+		tx: ITransactionContext,
 		queueParams: QueueParams
 	): Promise<Result<void, Error>>;
 }
