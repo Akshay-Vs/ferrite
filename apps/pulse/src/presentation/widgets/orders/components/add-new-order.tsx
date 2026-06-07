200dac3644c74552f826fc22ceb539a415cac9f2
Akshay-Vs <akvs.dev@gmail.com>
2026-06-07 23:42:35 +0530

    feat: create AddNewOrder component with button for adding new orders

---
+++ apps/pulse/src/presentation/widgets/orders/components/add-new-order.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/presentation/primitives/button';
+
+const AddNewOrder = () => {
+	return (
+		<Button size="lg" className="bg-active gradient-border">
+			Add new order
+		</Button>
+	);
+};
+
+export default AddNewOrder;
