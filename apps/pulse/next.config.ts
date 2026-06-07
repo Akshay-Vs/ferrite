9b431159eb6a48c82384db304ae8011ec3039190
Akshay-Vs <akvs.dev@gmail.com>
2026-06-07 23:38:17 +0530

    refactor: restructure next.config.ts to use dynamic configuration based on environment phase

--- apps/pulse/next.config.ts
+++ apps/pulse/next.config.ts
@@ -1,28 +1,71 @@
-import type { NextConfig } from 'next';
+import {
+	PHASE_DEVELOPMENT_SERVER,
+	PHASE_PRODUCTION_BUILD,
+} from 'next/constants.js';
+import path from 'path';
 
-const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'img.clerk.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'images.pexels.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'ik.imagekit.io',
-				port: '',
-				pathname: '/0ujcywips/**',
-			},
-		],
-	},
-};
+export default function config(phase: string) {
+	const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+	const isProd = phase === PHASE_PRODUCTION_BUILD;
 
-export default nextConfig;
+	const common = {
+		reactStrictMode: true,
+		images: {
+			remotePatterns: [
+				{
+					protocol: 'https',
+					hostname: 'img.clerk.com',
+					port: '',
+					pathname: '/**',
+				},
+				{
+					protocol: 'https',
+					hostname: 'ik.imagekit.io',
+					port: '',
+					pathname: '/0ujcywips/**',
+				},
+			],
+		},
+	};
+
+	const devConfig = {
+		images: {
+			remotePatterns: [
+				...common.images.remotePatterns,
+				// Test only
+				{
+					protocol: 'https',
+					hostname: 'i.pravatar.cc',
+					port: '',
+					pathname: '/**',
+				},
+				{
+					protocol: 'https',
+					hostname: 'images.pexels.com',
+					port: '',
+					pathname: '/**',
+				},
+			],
+		},
+		turbopack: {
+			debugIds: false,
+			root: path.join(__dirname, '../..'),
+		},
+		experimental: {
+			preloadEntriesOnStart: false,
+		},
+		typescript: {
+			ignoreBuildErrors: true, // run tsc separately
+		},
+	};
+
+	const prodConfig = {
+		productionBrowserSourceMaps: false,
+	};
+
+	return {
+		...common,
+		...(isDev ? devConfig : {}),
+		...(isProd ? prodConfig : {}),
+	};
+}
