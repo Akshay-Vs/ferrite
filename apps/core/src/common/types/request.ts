cf2cdf6f414fc2451a44fa0c752761cfb7a8afde
Akshay-Vs <akvs.dev@gmail.com>
2026-06-16 19:50:58 +0530

    feat: enhance the request type to include authentication stategy types

--- apps/core/src/common/types/request.ts
+++ apps/core/src/common/types/request.ts
@@ -1,4 +1,5 @@
 import { AuthUser } from '@ferrite/schema/auth/index';
+import { StorefrontUser } from '@ferrite/schema/auth/storefront-user.zodschema';
 import { WebhookEnvelope } from '@ferrite/schema/common/webhook-envelope.zodschema';
 import { Request as ExpressRequest } from 'express';
 
@@ -6,9 +7,19 @@ export interface Request extends ExpressRequest {
 	rawBody: Buffer;
 }
 
-export interface AuthenticatedRequest extends Request {
+export interface PlatformAuthenticatedRequest extends Request {
+	__authRealm: 'platform';
 	authUser: AuthUser;
 }
+
+export interface StorefrontAuthenticatedRequest extends Request {
+	__authRealm: 'storefront';
+	storefrontUser: StorefrontUser;
+}
+
+export type AuthenticatedRequest =
+	| PlatformAuthenticatedRequest
+	| StorefrontAuthenticatedRequest;
 
 export interface WebhookRequest extends Request {
 	webhookPayload?: WebhookEnvelope;
