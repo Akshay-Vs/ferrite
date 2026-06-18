79c54fc33ef84e226a506a68b85680286e0b3e6f
Akshay-Vs <akvs.dev@gmail.com>
2026-06-19 00:07:35 +0530

    feat: enhance StorefrontAuthModule with user repository and use cases

--- apps/core/src/modules/storefront-auth/storefront-auth.module.ts
+++ apps/core/src/modules/storefront-auth/storefront-auth.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { LoginUseCase } from './application/use-cases/login.usecase';
+import { RegisterUserUseCase } from './application/use-cases/register-user.usecase';
+import { STOREFRONT_LOGIN_UC } from './domain/ports/login-usecase.port';
+import { STOREFRONT_PASSWORD_HASHER } from './domain/ports/password-hasher.port';
+import { STOREFRONT_REGISTER_UC } from './domain/ports/register-usecase.port';
+import { STOREFRONT_USER_REPOSITORY } from './domain/ports/storefront-user-repository.port';
 import {
 	Argon2OptionsProvider,
 	Argon2Provider,
 } from './infrastructure/crypto/argon2.provider';
+import { Argon2PasswordHasher } from './infrastructure/crypto/password-hasher';
+import { StorefrontAuthController } from './infrastructure/http/controllers/storefront-auth.controller';
+import { DrizzleStorefrontUserRepository } from './infrastructure/persistance/repositories/drizzle-storefront-user.repository';
 
 @Module({
-	providers: [Argon2Provider, Argon2OptionsProvider],
+	controllers: [StorefrontAuthController],
+	providers: [
+		Argon2Provider,
+		Argon2OptionsProvider,
+		{
+			provide: STOREFRONT_USER_REPOSITORY,
+			useClass: DrizzleStorefrontUserRepository,
+		},
+		{
+			provide: STOREFRONT_PASSWORD_HASHER,
+			useClass: Argon2PasswordHasher,
+		},
+		{
+			provide: STOREFRONT_REGISTER_UC,
+			useClass: RegisterUserUseCase,
+		},
+		{
+			provide: STOREFRONT_LOGIN_UC,
+			useClass: LoginUseCase,
+		},
+	],
 })
 export class StorefrontAuthModule {}
