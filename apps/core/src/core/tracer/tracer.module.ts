7a0683bda35e1285076bd543cfca377b22e807ce
Akshay Vs <akvsx@proton.me>
2026-09-09 11:06:35 +0530

    feat(tracer): integrate ConfigService for dynamic tracing configuration

--- apps/core/src/core/tracer/tracer.module.ts
+++ apps/core/src/core/tracer/tracer.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OTEL_TRACER } from './tracer.constraint';
 import { TracerService } from './tracer.service';
 
 @Global()
 @Module({
+	imports: [ConfigModule],
 	providers: [
 		{
 			provide: OTEL_TRACER,
